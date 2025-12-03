1e2075391355ba98a0085df2317e8ab2551a241f
HShrigma <hristostantchev@abv.bg>
2025-12-03 15:19:44 +0200

    feat(tasklist): implement basic taskList creation - added a placeholder button - button updates allTasks state to add a new taskList - add kinetic feedback on taskList add/delete

--- src/view/tasks/TasksHolder.tsx
+++ src/view/tasks/TasksHolder.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import TaskList, { type TaskListData } from "./TaskList";
 import Masonry from "react-masonry-css";
-import { triggerScreenBob } from "../utils/screenShake";
+import { triggerScreenBob, triggerScreenShake } from "../utils/screenShake";
 
 export const TasksHolder = () => {
   const breakpointColumnsObj = {
@@ -19,8 +19,9 @@ export const TasksHolder = () => {
     { id: 1, title: "tasks 1", tasks: [...sampleTasks] },
     { id: 2, title: "tasks 2", tasks: [...sampleTasks] }
   ]);
+  const [maxId,setMaxId] = useState(3);
 
-  const getTaskListIndexById = (id:number) => {
+  const getTaskListIndexById = (id: number) => {
     return allTasks.findIndex(t => t.id === id);
   }
   const handleTaskChanged = (id: number, taskId: number, label: string, completed: boolean) => {
@@ -28,15 +29,15 @@ export const TasksHolder = () => {
     const newTaskList = { ...allTasks[listIndex] };
     newTaskList.tasks[newTaskList.tasks.findIndex(t => t.id === taskId)] = { id: taskId, label, done: completed };
 
-    setAllTasks(prev => prev.map( t => (t.id === id ? newTaskList : t)));
+    setAllTasks(prev => prev.map(t => (t.id === id ? newTaskList : t)));
   };
 
   const addNewTask = (id: number, newTaskId: number, label: string) => {
     const listIndex = getTaskListIndexById(id);
     const newTaskList = { ...allTasks[listIndex] };
-    
+
     newTaskList.tasks.push({ id: newTaskId, label, done: false });
-    setAllTasks(prev => prev.map( t => (t.id === id ? newTaskList : t)));
+    setAllTasks(prev => prev.map(t => (t.id === id ? newTaskList : t)));
     triggerScreenBob(150);
   };
 
@@ -50,43 +51,56 @@ export const TasksHolder = () => {
     );
   };
 
-  const removeList = (id: number) => {
-    const newTasks = [...allTasks];
-    setAllTasks(newTasks.filter(task => task.id != id));
-  }
-
   const editTaskTitle = (id: number, newValue: string) => {
     const newTasks = [...allTasks];
     newTasks[getTaskListIndexById(id)].title = newValue;
     setAllTasks(newTasks);
   };
 
-  const submitTaskTitle = () => { 
+  const removeList = (id: number) => {
+    const newTasks = [...allTasks];
+    setAllTasks(newTasks.filter(task => task.id != id));
+    triggerScreenShake();
+  }
+
+  const submitTaskTitle = () => {
     triggerScreenBob(200);
   };
+
+  const addTaskList = () =>{
+    const newTasks = [...allTasks];
+    newTasks.push({id:maxId, title: "tasks "+maxId, tasks: [...sampleTasks]});
+    setMaxId(n => n + 1);
+    setAllTasks(newTasks);
+    triggerScreenBob();
+  }
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className="flex gap-4"
-      columnClassName="flex flex-col gap-4"
-    >
-      {allTasks.map(task =>
-        <section 
-          className="bg-[#1f2335] p-5 rounded-md shadow-md shadow-black/30 border border-[#2a2f47]"
-          key={task.id}
-        >
-          <TaskList
-            data={task}
-            onTaskChanged={handleTaskChanged}
-            onTaskAdded={addNewTask}
-            onTaskRemoved={removeTask}
-            onTitleEdited={editTaskTitle}
-            onTitleSubmitted={submitTaskTitle}
-            onDeleted={removeList}
-          />
-        </section>
-      )}
-    </Masonry>
+    <div>
+      <button onClick={addTaskList}> Add List </button>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="flex gap-4"
+        columnClassName="flex flex-col gap-4"
+      >
+        {allTasks.map(task =>
+          <section
+            className="bg-[#1f2335] p-5 rounded-md shadow-md shadow-black/30 border border-[#2a2f47]"
+            key={task.id}
+          >
+            <TaskList
+              data={task}
+              onTaskChanged={handleTaskChanged}
+              onTaskAdded={addNewTask}
+              onTaskRemoved={removeTask}
+              onTitleEdited={editTaskTitle}
+              onTitleSubmitted={submitTaskTitle}
+              onDeleted={removeList}
+            />
+          </section>
+        )}
+      </Masonry>
+
+    </div>
   );
 };
 
