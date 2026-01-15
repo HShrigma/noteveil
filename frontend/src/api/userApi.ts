b64c1b2cad92d3985d5453820c84f570fad4e112
HShrigma <hristostantchev@abv.bg>
2026-01-15 12:11:59 +0200

    feat(api): implement user API

---
+++ frontend/src/api/userApi.ts
@@ -0,0 +1,48 @@
+import { CORE_URL } from "./apiUtils"
+const BASE_URL = `${CORE_URL}/user`;
+
+// Get User
+export const fetchUser = async (id: number) => {
+    const res = await fetch(`${BASE_URL}/${id}`);
+    return res.json();
+}
+
+// Delete User
+export const deleteUser = async (id: number) => {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "DELETE"
+    });
+
+    if (!res.ok) throw new Error("Failed to delete user");
+    return await res.json();
+}
+
+
+// Add User
+export const addUser = async (name: string) => {
+    const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: name })
+    })
+
+    if (!res.ok) throw new Error("Failed to add new user");
+
+    return await res.json();
+
+}
+
+// Update User
+export const patchUser = async (id: number, key: string, value: string) => {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            [key]: value
+        })
+    })
+
+    if (!res.ok) throw new Error("Couldn't patch username");
+
+    return await res.json();
+}
