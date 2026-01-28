2cda0f83dc24224a0d05b04377eb5f622208e144
HShrigma <hristostantchev@abv.bg>
2026-01-28 11:13:47 +0200

    feat(icon): implement noteveil logo to replace placeholder

--- frontend/src/components/header/compositional/HeaderTop.tsx
+++ frontend/src/components/header/compositional/HeaderTop.tsx
@@ -1,5 +1,7 @@
 import { useUserContext } from "../../../context/users/userContext";
 import User from "./user/User";
+import logo from '../../../assets/logo.svg';
+
 interface HeaderTopProps {
     onLogout: (withMessage?: boolean) => void;
 }
@@ -9,8 +11,8 @@ const HeaderTop = ({  onLogout, }: HeaderTopProps) => {
     return (
         <div className={`flex items-center ${!ctx.isUserLoggedIn() ? "justify-center" : "justify-between"}`}>
             {/* Title */}
-            <h1 className={`text-3xl md:text-4xl font-bold tracking-wider text-purple-400 ${!ctx.isUserLoggedIn() ? "fade-in scale-105" : "slide-left"}`}>
-                Noteveil
+            <h1 className={`flex items-center text-3xl md:text-4xl font-bold tracking-wider   bg-gradient-to-b from-[#8f44cfff] to-purple-400 bg-clip-text text-transparent ${!ctx.isUserLoggedIn() ? "fade-in scale-105" : "slide-left"}`}>
+                <img draggable={false} src={logo} alt="N" className="cursor-text inline w-15 h-15 mr-2" />oteveil
             </h1>
 
             {/* User */}
