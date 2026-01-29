4d692a0b73c7e515345acb110c2eaf6f34f0fdf6
HShrigma <hristostantchev@abv.bg>
2026-01-29 14:57:06 +0200

    fix(style): add self-start to error hint

--- frontend/src/components/shared/ErrorHint.tsx
+++ frontend/src/components/shared/ErrorHint.tsx
@@ -18,7 +18,7 @@ export const ErrorHint = ({ message, toValidate, triggerCheck, className = "" }:
      }, [triggerCheck, toValidate]);
 
     if (isValid) return <></>;
-    return <div className={`error-hint ${className}`}> {message}</div>;
+    return <div className={`self-start error-hint ${className}`}> {message}</div>;
             
 };
 
