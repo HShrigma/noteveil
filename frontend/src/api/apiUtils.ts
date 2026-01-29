fb9939fbdec1e5e9e8aa1dac541c815dabc5cabb
HShrigma <hristostantchev@abv.bg>
2026-01-29 14:35:06 +0200

    feat(env): make frontend API base connect from env

--- frontend/src/api/apiUtils.ts
+++ frontend/src/api/apiUtils.ts
@@ -1,3 +1,3 @@
-export const CORE_URL = "http://localhost:4000/api";
+export const CORE_URL = import.meta.env.VITE_API_BASE || "http://localhost:4000/api";
 export const PROJECTS_URL = `${CORE_URL}/projects`;
 export const USERS_URL = `${CORE_URL}/users`;
